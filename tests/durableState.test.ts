15f03b49d35761da5cf3cebd487e4770a091e8af
Tin Nguyen <tinnhlt@blockchainlabs.asia>
2024-07-27 18:22:16 +0700

    update test

--- tests/durableState.test.ts
+++ tests/durableState.test.ts
@@ -1,5 +1,11 @@
 import { test, expect, mock } from "bun:test";
-import { DurableState, type StepHandler, type StepIt } from "../src";
+import {
+  DurableState,
+  type ContinueTrigger,
+  type DurableStateIterator,
+  type StepHandler,
+  type StepIt,
+} from "../src";
 import * as TestHelper from "./helper";
 
 test("simple_durable_state", async () => {
@@ -88,4 +94,188 @@ test("simple_durable_state", async () => {
   expect(TestHelper.removeProps(ins.auditLogs, ["_at"])).toMatchSnapshot(
     "audit_logs"
   );
+});
+
+test("step_durable_state", async () => {
+  enum EStep {
+    step_begin = "step_begin",
+    step_compute = "step_compute",
+    step_end = "step_end",
+  }
+  type TStateShape = Partial<{
+    startHit: boolean;
+    computed: number;
+    endHit: boolean;
+  }>;
+  type EAuditLog = "start_hit" | "end_hit" | "computed_hit";
+
+  class SampleState extends DurableState<EStep, TStateShape, EAuditLog> {
+    // Type Safeguard & Auto register
+    private _static(key: EStep): StepHandler<EStep> {
+      return this[key];
+    }
+    private _collectAndRegisterSteps() {
+      Object.values(EStep).map((step) =>
+        this.stepHandler.set(step, this._static(step).bind(this))
+      );
+    }
+    constructor() {
+      super(EStep.step_begin, {
+        withAuditLog: true,
+      });
+      this._collectAndRegisterSteps();
+    }
+
+    private async *step_begin(): StepIt<EStep> {
+      this.state.startHit = true;
+      this.addLog({
+        type: "start_hit",
+        values: {},
+      });
+      return { nextStep: EStep.step_compute };
+    }
+
+    private async *step_compute(): StepIt<EStep> {
+      let sum = 0;
+      for (let i = 0; i < 3; i++) {
+        // with cached action
+        const { it, value } = await this.withAction(
+          `cache_computed_${i}`,
+          async () => {
+            return Math.PI;
+          }
+        );
+        if (it) yield it;
+        sum += value;
+      }
+
+      this.addLog({
+        type: "computed_hit",
+        values: {
+          actionValue: sum,
+        },
+      });
+      this.state.computed = sum;
+
+      return { nextStep: EStep.step_end };
+    }
+    private async *step_end(): StepIt<EStep> {
+      this.state.endHit = true;
+      this.addLog({
+        type: "end_hit",
+        values: {},
+      });
+      return { nextStep: null };
+    }
+  }
+
+  // Test
+  const ins = DurableState.fromJSON<EStep, TStateShape, EAuditLog, SampleState>(
+    SampleState,
+    {
+      step: EStep.step_begin,
+      state: {
+        computed: 0,
+      },
+    }
+  );
+
+  // 1st run
+  const it = ins.exec();
+  let res = await it.next();
+  expect(res.done).toBe(false);
+  let snapshot = TestHelper.removeProps(ins.toJSON(), ["_at"]);
+  expect(snapshot).toMatchSnapshot("run_part_1");
+
+  // 2nd run
+  res = await it.next();
+  res = await it.next();
+  res = await it.next();
+  expect(res.done).toBe(true);
+  snapshot = TestHelper.removeProps(ins.toJSON(), ["_at"]);
+  expect(snapshot).toMatchSnapshot("run_part_2");
+});
+
+test("wait_ms_durable_state", async () => {
+  enum EStep {
+    step_begin = "step_begin",
+    step_end = "step_end",
+  }
+  type TStateShape = Partial<{
+    startHit: boolean;
+    endHit: boolean;
+  }>;
+  type EAuditLog = "log";
+
+  class SampleState extends DurableState<EStep, TStateShape, EAuditLog> {
+    // Type Safeguard & Auto register
+    private _static(key: EStep): StepHandler<EStep> {
+      return this[key];
+    }
+    private _collectAndRegisterSteps() {
+      Object.values(EStep).map((step) =>
+        this.stepHandler.set(step, this._static(step).bind(this))
+      );
+    }
+    constructor() {
+      super(EStep.step_begin, {
+        withAuditLog: true,
+      });
+      this._collectAndRegisterSteps();
+    }
+
+    private async *step_begin(): StepIt<EStep> {
+      this.state.startHit = true;
+      const { it } = this.waitForMs("wait_for_1sec", 1000);
+      if (it) yield it;
+      this.addLog({
+        type: "log",
+        values: {
+          msg: "after 1 sec",
+        },
+      });
+
+      return { nextStep: EStep.step_end };
+    }
+
+    private async *step_end(): StepIt<EStep> {
+      this.state.endHit = true;
+
+      return { nextStep: null };
+    }
+  }
+
+  // Test
+  const ins = DurableState.fromJSON<EStep, TStateShape, EAuditLog, SampleState>(
+    SampleState,
+    {
+      step: EStep.step_begin,
+      state: {},
+    }
+  );
+
+  (ins as any).genResumeId = (key: string) => `${key}-1`;
+  const it = ins.exec();
+  let res = await it.next();
+
+  // not finish - have a continue trigger
+  expect(res.done).toBe(false);
+  const resume = (res.value as DurableStateIterator<EStep>)
+    .resumeTrigger as ContinueTrigger;
+  expect(resume).not.toBeNull();
+  expect(TestHelper.removeProps(resume, ["resumeAt"])).toMatchSnapshot();
+
+  // when time on
+  ins.resolveResume(resume.resumeId, {});
+
+  res = await it.next();
+  expect(res.done).toBe(true);
+
+  // snapshot check
+  let snapshot = TestHelper.removeProps(ins.toJSON(), [
+    "_at",
+    "resumeAt",
+    "resumeAfter",
+  ]);
+  expect(snapshot).toMatchSnapshot("durable_state_with_waiting");
 });
